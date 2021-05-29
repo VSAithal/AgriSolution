8b6484040a6ba04fee218822feb68c35a8233ffa
Vidyasagar <aithalrvs10@gmail.com>
2021-05-29 18:28:54 +0200

    Cors error solution

--- src/services/getPlantDetails.js
+++ src/services/getPlantDetails.js
@@ -1,13 +1,18 @@
 import axios from "axios";
-export const baseURL = `http://dev.api.agrilution.com/plantData.json`;
-// export const baseURL = `https://dev.api.agrilution.com/plantData2.json`;
+// export const baseURL = `http://dev.api.agrilution.com/plantData.json`;
+export const baseURL = `https://dev.api.agrilution.com/plantData2.json`;
 export const newPlantDetailsURL = `${baseURL}`;
 // async function always returns a promise with data
 // Promise call to fetch the data from the API
 export const getPlantsDetails = async () => {
   // destructuring the the API fetched data by using {data}
   const result = await axios
-    .get(`${newPlantDetailsURL}`)
+    .get(`${newPlantDetailsURL}`,{ headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      },
+      responseType: 'json'
+    })
     .then(({ data }) => data && data);
   return result;
 };
